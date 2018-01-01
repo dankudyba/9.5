191283d144e24a219f8c8882b8569178e5466c7e
Daniel Kudyba <dankudyba@gmail.com>
2018-01-01 18:34:19 +0100

    Add solution, change style, index, script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-console.log(window);
+var btn = document.getElementsByClassName('button');
+
+for(var i=0; i<=btn.length; i++) {
+	alert(btn.innerText = document.getElementsByClassName('button')[i].textContent);
+}
